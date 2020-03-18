692db6010bc157a7405cec7521d7dc69c968fd36
Aemaeth <aemaeth@RoyAylas-MacBook-Pro.local>
2020-03-18 11:30:48 +0800

    init test

--- src/emqx_plugin_demo.erl
+++ src/emqx_plugin_demo.erl
@@ -14,7 +14,7 @@
 %% limitations under the License.
 %%--------------------------------------------------------------------
 
--module(emqx_plugin_template).
+-module(emqx_plugin_demo).
 
 -include_lib("emqx/include/emqx.hrl").
 
